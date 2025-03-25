1a62b1323f1f87858d67022a102be0f85711b258
Ilse <magi@gmail.com>
2025-03-24 21:54:13 -0700

    adding firebase config

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ite_app/colors.dart';
 
 class Register extends StatefulWidget {
@@ -9,16 +12,13 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  final _formKey = GlobalKey<FormState>();
-
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _controlNumberController = TextEditingController();
-
   String? _selectedProgram;
-  bool _obscurePassword = true;
+  String? _errorMessage;
 
   final List<String> _programs = [
     'Ingeniería en Sistemas Computacionales',
@@ -34,21 +34,47 @@ class _RegisterState extends State<Register> {
     return email.endsWith('@ite.edu.mx');
   }
 
-  void _register() {
-    if (_formKey.currentState!.validate()) {
-      // Si todo está validado, proceder con el registro.
-      Navigator.pushReplacementNamed(context, '/login');
-    }
-  }
+  Future<void> _register() async {
+    String firstName = _firstNameController.text;
+    String lastName = _lastNameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    String controlNumber = _controlNumberController.text;
 
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    _firstNameController.dispose();
-    _lastNameController.dispose();
-    _controlNumberController.dispose();
-    super.dispose();
+    if (firstName.isEmpty || lastName.isEmpty || controlNumber.isEmpty || email.isEmpty || password.isEmpty || _selectedProgram == null) {
+      setState(() {
+        _errorMessage = 'Por favor, complete todos los campos requeridos.';
+      });
+      return;
+    }
+
+    if (!_isEmailValid(email)) {
+      setState(() {
+        _errorMessage = 'El correo electrónico debe terminar con @ite.edu.mx';
+      });
+      return;
+    }
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+        'controlNumber': controlNumber,
+        'program': _selectedProgram,
+      });
+
+      Navigator.pushReplacementNamed(context, '/login');
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'Error al registrar: ${e.toString()}';
+      });
+    }
   }
 
   @override
@@ -58,125 +84,101 @@ class _RegisterState extends State<Register> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20),
-                Text(
-                  'Crear Cuenta',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: AppColors.primary),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Text('Crear Cuenta', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: AppColors.primary)),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(child: _buildTextField(_firstNameController, 'Nombre')),
+                  SizedBox(width: 10),
+                  Expanded(child: _buildTextField(_lastNameController, 'Apellido')),
+                ],
+              ),
+              SizedBox(height: 15),
+              _buildTextField(_controlNumberController, 'Número de Control'),
+              SizedBox(height: 15),
+              _buildTextField(_emailController, 'Correo Electrónico'),
+              SizedBox(height: 15),
+              _buildTextField(_passwordController, 'Contraseña', obscureText: true),
+              SizedBox(height: 15),
+              _buildDropdown(),
+              SizedBox(height: 30),
+              _buildRegisterButton(),
+              if (_errorMessage != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(_errorMessage!, style: TextStyle(color: Colors.red)),
                 ),
-                const SizedBox(height: 30),
-                Row(
-                  children: [
-                    Expanded(child: _buildTextField(_firstNameController, 'Nombre')),
-                    const SizedBox(width: 10),
-                    Expanded(child: _buildTextField(_lastNameController, 'Apellido')),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                _buildTextField(_controlNumberController, 'Número de Control'),
-                const SizedBox(height: 15),
-                _buildDropdown(),
-                const SizedBox(height: 15),
-                _buildTextField(_emailController, 'Correo Electrónico', validator: (value) {
-                  if (value == null || value.isEmpty) return 'Campo requerido';
-                  if (!_isEmailValid(value)) return 'Debe usar un correo @ite.edu.mx';
-                  return null;
-                }),
-                const SizedBox(height: 15),
-                _buildPasswordTextField(),
-                
-                const SizedBox(height: 30),
-                _buildRegisterButton(),
-              ],
-            ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget _buildTextField(TextEditingController controller, String label, {String? Function(String?)? validator}) {
-    return TextFormField(
+  Widget _buildTextField(TextEditingController controller, String label, {bool obscureText = false}) {
+    return TextField(
       controller: controller,
+      obscureText: obscureText,
       decoration: InputDecoration(
         labelText: label,
         labelStyle: TextStyle(color: AppColors.primary),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-        filled: true,
-        fillColor: AppColors.background,
-      ),
-      validator: validator ?? (value) {
-        if (value == null || value.isEmpty) return 'Campo requerido';
-        return null;
-      },
-    );
-  }
-
-  Widget _buildPasswordTextField() {
-    return TextFormField(
-      controller: _passwordController,
-      obscureText: _obscurePassword,
-      decoration: InputDecoration(
-        labelText: 'Contraseña',
-        labelStyle: TextStyle(color: AppColors.primary),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-        filled: true,
-        fillColor: AppColors.background,
-        suffixIcon: IconButton(
-          icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility, color: AppColors.primary),
-          onPressed: () => setState(() => _obscurePassword = !_obscurePassword),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColors.primary),
         ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColors.primary, width: 2),
+        ),
+        filled: true,
+        fillColor: AppColors.background,
       ),
-      validator: (value) {
-        if (value == null || value.isEmpty) return 'Campo requerido';
-        if (value.length < 6) return 'Debe tener al menos 6 caracteres';
-        return null;
-      },
     );
   }
 
   Widget _buildDropdown() {
-  return DropdownButtonFormField<String>(
-    value: _selectedProgram, // Se asegura de que el valor seleccionado sea visible
-    onChanged: (String? newValue) {
-      setState(() {
-        _selectedProgram = newValue; // Actualiza el estado con la nueva selección
-      });
-    },
-    items: _programs.map<DropdownMenuItem<String>>((String program) {
-      return DropdownMenuItem<String>(
-        value: program,
-        child: Text(program, style: TextStyle(color: AppColors.primary)),
-      );
-    }).toList(),
-    isExpanded: true,
-    decoration: InputDecoration(
-      labelText: 'Seleccione una carrera', // Se usa labelText en lugar de hint
-      labelStyle: TextStyle(color: AppColors.primary),
-      filled: true,
-      fillColor: AppColors.background,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(10),
-        borderSide: BorderSide(color: AppColors.primary),
+    return DropdownButtonFormField<String>(
+      value: _selectedProgram,
+      hint: Text('Seleccione una carrera', style: TextStyle(color: AppColors.primary)),
+      onChanged: (String? newValue) {
+        setState(() {
+          _selectedProgram = newValue;
+        });
+      },
+      items: _programs.map<DropdownMenuItem<String>>((String program) {
+        return DropdownMenuItem<String>(
+          value: program,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+            child: Text(program, style: TextStyle(color: AppColors.primary)),
+          ),
+        );
+      }).toList(),
+      isExpanded: true,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: AppColors.background,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColors.primary),
+        ),
       ),
-    ),
-  );
-}
-
+    );
+  }
 
   Widget _buildRegisterButton() {
     return ElevatedButton(
       onPressed: _register,
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
-        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
-      child: const Text('Registrar', style: TextStyle(color: AppColors.secondary, fontSize: 16)),
+      child: Text('Registrar', style: TextStyle(color: AppColors.secondary, fontSize: 16)),
     );
   }
 }
