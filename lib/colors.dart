7d08925c84514b05053eae395ee452c62c1502ec
Ilse <magi@gmail.com>
2025-03-24 22:06:42 -0700

    final touches done, we need to run test

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2B3A67);
+  static const Color secondary = Color(0xFFD8C495);
+  static const Color background = Color(0xFFF2F6F7);
+  static const Color accent = Color(0xFFBFBFBF);
+  static const Color lightBlue = Color(0xFFB4DCE3);
+}
