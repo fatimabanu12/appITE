b487728d8d5a0aded7588cd57714f07b79278249
Ilse <magi@gmail.com>
2025-03-25 12:58:44 -0700

    fixing firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:ite_app/pages/home_page.dart';
 import 'package:ite_app/pages/login.dart';
 import 'package:ite_app/pages/register.dart';
 import 'package:ite_app/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
